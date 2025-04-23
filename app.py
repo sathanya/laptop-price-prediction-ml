58c0662f9aa50dc298964837770def9f8f5aa272
sathanya <kavikum41@gmail.com>
2025-04-23 10:53:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,6 +40,4 @@ def index():
     return render_template('index.html', prediction=prediction)
 
 if __name__ == '__main__':
-    
-    app.run(host='0.0.0.0', port=5050)
-
+    app.run(host='0.0.0.0', port=5000)
